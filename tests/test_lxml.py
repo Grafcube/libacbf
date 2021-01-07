f44865cbbfa36e4ff4c48fc1bb7d6db469a0aa54
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-01-07 18:55:34 +0530

    Updated test

--- tests/test_lxml.py
+++ tests/test_lxml.py
@@ -8,7 +8,9 @@ ACBFns = r"{http://www.fictionbook-lib.org/xml/acbf/1.0}"
 with open(sample, "r") as book:
 	root = etree.fromstring(bytes(book.read(), encoding="utf-8"))
 	tree = etree.ElementTree(root)
-	for i in list(root.find(f"{ACBFns}meta-data/{ACBFns}book-info")):
-		print(i.tag.replace(ACBFns, ""))
-	for i in list(root.find(f"{ACBFns}meta-data/{ACBFns}book-info/{ACBFns}author")):
-		print(i.tag.replace(ACBFns, ""))
+	# for i in list(root.find(f"{ACBFns}meta-data/{ACBFns}book-info")):
+	# 	print(i.tag.replace(ACBFns, ""))
+	# for i in list(root.find(f"{ACBFns}meta-data/{ACBFns}book-info/{ACBFns}author")):
+	# 	print(i.tag.replace(ACBFns, ""))
+	for i in list(root.findall(f"{ACBFns}meta-data/{ACBFns}book-info/{ACBFns}author")):
+		print(i.attrib)
