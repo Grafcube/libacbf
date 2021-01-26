14d2b9ff627b98d0e6d0e026af5bb86208f251f2
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-01-26 19:35:30 +0530

    Updated TextArea

--- libacbf/BodyInfo.py
+++ libacbf/BodyInfo.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from re import split
+from lxml import etree
 from libacbf.Constants import PageTransitions, TextAreas
 
 class Page:
@@ -7,7 +8,7 @@ class Page:
 	docstring
 	"""
 	def __init__(self):
-		self.bg_colour = None
+		self.bg_color = None
 
 		self.transition = PageTransitions.fade
 
@@ -17,44 +18,75 @@ class Page:
 
 		self.text_layers = {}
 
-		self.frames = [{
-			"points": [()],
-			"bgcolor": None
-		}]
+		self.frames = []
 
-		self.jumps = [{
-			"page": 0,
-			"points": [()]
-		}]
+		self.jumps = []
+
+	def dict(self):
+		return {
+			"bg_color": self.bg_color,
+			"transition": str(self.transition),
+			"title": self.title,
+			"image_ref": self.image_ref,
+			"text_layers": self.text_layers,
+			"frames": self.frames,
+			"jumps": self.jumps
+		}
 
 class TextLayer:
 	"""
 	docstring
 	"""
-	def __init__(self):
-		self.language = ""
+	def __init__(self, layer: etree._Element, ACBFns: str):
+		self.language = layer.attrib["lang"]
 
-		self.points = [()]
+		if "bgcolor" in layer.keys():
+			self.bg_colour = layer.attrib["bgcolor"]
 
-		self.text = ""
+		self.text_areas = []
+		areas = layer.findall(f"{ACBFns}text-area")
+		for ar in areas:
+			self.text_areas.append(TextArea(ar, ACBFns))
+
+class TextArea:
+	"""
+	docstring
+	"""
+	def __init__(self, area: etree._Element, ACBFns: str):
+		self.points = get_points(area.attrib["points"])
+
+		self.paragraph = []
+		for p in area.findall(f"{ACBFns}p"):
+			self.paragraph.append(etree.tostring(p, encoding="utf-8"))
 
 		# Optional
 		self.bg_colour = None
+		if "bgcolor" in area.keys():
+			self.bg_colour = area.attrib["bgcolor"]
 
 		self.rotation = 0
+		if "text-rotation" in area.keys():
+			self.rotation = area.attrib["text-rotation"]
 
 		self.type = TextAreas.Speech
+		if "type" in area.keys():
+			self.rotation = area.attrib["type"]
 
 		self.inverted = False
+		if "inverted" in area.keys():
+			self.rotation = area.attrib["inverted"]
 
 		self.transparent = False
-
-		self.paragraph = ""
+		if "transparent" in area.keys():
+			self.rotation = area.attrib["transparent"]
 
 def get_textlayers(item, ACBFns):
+	text_layers = {}
 	textlayer_items = item.findall(f"{ACBFns}text-layer")
 	for lr in textlayer_items:
-		pass
+		new_lr = TextLayer(lr)
+		text_layers[new_lr.language] = new_lr
+	return text_layers
 
 def get_frames(item, ACBFns):
 	frames = []
@@ -72,10 +104,7 @@ def get_frames(item, ACBFns):
 		}
 		frames.append(frame)
 
-	if len(frames) > 1 or len(frames) == 0:
-		return frames
-	else:
-		return frames[0]
+	return frames
 
 def get_jumps(item, ACBFns):
 	jumps = []
@@ -89,14 +118,13 @@ def get_jumps(item, ACBFns):
 		}
 		jumps.append(jump)
 
-	if len(jumps) > 1 or len(jumps) == 0:
-		return jumps
-	else:
-		return jumps[0]
+	return jumps
 
 def get_points(pts_str: str):
-	pts = split(pts_str)
-	for pt in pts_str:
+	pts = []
+	pts_l = split(" ", pts_str)
+	for pt in pts_l:
 		ls = split(",", pt)
-		pts.append(namedtuple((int(ls[0]), int(ls[1])), "x y"))
+		vec2 = namedtuple("Vector2", "x y")
+		pts.append( vec2( int(ls[0]), int(ls[1]) ) )
 	return pts
--- tests/test_lxml.py
+++ tests/test_lxml.py
@@ -1,20 +1,12 @@
-import os
 from lxml import etree
+from libacbf.BodyInfo import TextArea
 
 sample = "tests/samples/Doctorow, Cory - Craphound-1.1.acbf"
 
 with open(sample, "r") as book:
 	root = etree.fromstring(bytes(book.read(), encoding="utf-8"))
 	ACBFns = r"{" + root.nsmap[None] + r"}"
-	# print(root.nsmap)
-	# for i in list(root.find(f"{ACBFns}meta-data")):
-	# 	print(i.tag.replace(ACBFns, ""))
-	# 	print(type(i))
-	# for i in root.findall(f"{ACBFns}meta-data/{ACBFns}document-info/{ACBFns}source/{ACBFns}p"):
-	# 	print(i.tag)
-	print(type(root.find("adfbh")))
 
-	# meta = root.find(f"{ACBFns}meta-data/{ACBFns}book-info/{ACBFns}characters")
-	# print(type(meta))
-	# for i in meta:
-	# 	print(i.tag)
+	tx_area = root.find(f"{ACBFns}body/{ACBFns}page/{ACBFns}text-layer/{ACBFns}text-area")
+
+	print(TextArea(tx_area, ACBFns).paragraph)
