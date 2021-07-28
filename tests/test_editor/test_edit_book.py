c50cd48e170a4a562395dde622364b6ab9813dda
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-07-28 18:12:05 +0530

    Updated edit tests Writing data to other archive types.

--- tests/test_editor/test_edit_book.py
+++ tests/test_editor/test_edit_book.py
@@ -8,6 +8,8 @@ os.makedirs(edit_dir, exist_ok=True)
 
 def test_references():
     with ACBFBook(edit_dir / "test_references.acbf", 'w', archive_type=None) as book:
+        book.Metadata.book_info.edit_title("Test Edit References")
+
         book.edit_reference("test_ref", "This is a new test reference.")
         book.edit_reference("electric_boogaloo", "This is another test reference.\nWith another line.")
         book.edit_reference("tb_deleted", "This one will be deleted.")
@@ -16,27 +18,32 @@ def test_references():
 
 
 def test_data():
-    with ACBFBook(edit_dir / "test_data.cbz", 'w') as book:
-        book.Data.add_data("tests/samples/assets/cover.jpg")
-        book.Metadata.book_info.cover_page.set_image_ref("cover.jpg")
+    for ext, type in (("cbz", "Zip"), ("cb7", "SevenZip"), ("cbt", "Tar")):
+        with ACBFBook(edit_dir / f"test_data.{ext}", 'w', type) as book:
+            book.Metadata.book_info.edit_title("Test Edit Data")
 
-        book.Data.add_data("tests/samples/assets/page1.jpg")
-        book.Body.pages[0].set_image_ref("page1")
+            book.Data.add_data("tests/samples/assets/cover.jpg")
+            book.Metadata.book_info.cover_page.set_image_ref("cover.jpg")
 
-        book.Data.add_data("tests/samples/assets/page2.jpg", embed=True)
-        book.Body.insert_new_page(1, "#page2.jpg")
+            book.Data.add_data("tests/samples/assets/page1.jpg")
+            book.Body.pages[0].set_image_ref("page1")
 
-        book.Data.add_data("tests/samples/assets/page3.jpg", "003.jpg")
-        book.Body.insert_new_page(2, "003.jpg")
+            book.Data.add_data("tests/samples/assets/page2.jpg", embed=True)
+            book.Body.insert_new_page(1, "#page2.jpg")
 
-        book.Data.add_data("tests/samples/assets/page4.jpg", "to_be_removed.jpg")
-        book.Data.add_data("tests/samples/assets/page4.jpg", "to_be_removed2.jpg", True)
-        book.Data.remove_data("to_be_removed.jpg")
-        book.Data.remove_data("to_be_removed2.jpg", embed=True)
+            book.Data.add_data("tests/samples/assets/page3.jpg", "003.jpg")
+            book.Body.insert_new_page(2, "003.jpg")
+
+            book.Data.add_data("tests/samples/assets/page4.jpg", "to_be_removed.jpg")
+            book.Data.add_data("tests/samples/assets/page4.jpg", "to_be_removed2.jpg", True)
+            book.Data.remove_data("to_be_removed.jpg")
+            book.Data.remove_data("to_be_removed2.jpg", embed=True)
 
 
 def test_styles():
     with ACBFBook(edit_dir / "test_styles.cbz", 'w') as book:
+        book.Metadata.book_info.edit_title("Test Edit Styles")
+
         book.Styles.edit_style("tests/samples/assets/styles/default.css", embed=True)
         book.Styles.edit_style("tests/samples/assets/styles/styles.css", "styles/to_be_removed.css")
         book.Styles.edit_style("tests/samples/assets/styles/sample.scss", type="text/x-scss")
