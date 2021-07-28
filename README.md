6d373b3dd34b47344825d35c8fba71661ed46df8
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-07-28 18:42:09 +0530

    Update setup scripts TODO Docs and CI

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # libacbf
-A Python library to manage ACBF ebooks.
+
+A Python library to read and edit ACBF formatted comic book files and archives.
 
 https://acbf.fandom.com/wiki/Advanced_Comic_Book_Format_Wiki
--- setup.py
+++ setup.py
@@ -3,9 +3,33 @@
 from setuptools import setup, find_packages
 
 setup(name="libacbf",
-	version='',
-	description='',
-	author="Grafcube",
-	license="BSD-3-Clause License",
-	url='',
-	packages=find_packages())
+      version='1.0.0',
+      description="A library to read and edit ACBF formatted comic book files and archives.",
+      author="Grafcube",
+      license="BSD-3-Clause License",
+      url="https://github.com/Grafcube/libacbf",
+      packages=find_packages(),
+      download_url="",  # TODO: After docs
+      keywords=[],
+      install_requires=[
+          "wheel",
+          "lxml",
+          "python-magic",
+          "py7zr",
+          "rarfile",
+          "requests",
+          "langcodes",
+          "python-dateutil"
+          ],
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3 :: Only",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Topic :: Software Development :: Libraries",
+          "Typing :: Typed"
+          ]
+      )
