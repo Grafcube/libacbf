10ba4f2e38d7c967a1bb20637a2643cfd0b7ea01
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-08-19 16:11:52 +0530

    Fixed book test

--- tests/test_book.py
+++ tests/test_book.py
@@ -9,14 +9,14 @@ from tests.testres import samples, fail
 
 @pytest.mark.parametrize("dir", samples.values())
 def test_book_props(dir):
-    book = ACBFBook(dir)
-    dir = Path("tests/results") / Path(dir).name
-    os.makedirs(dir, exist_ok=True)
-    op = {
-        "book_path": str(book.book_path),
-        "archive_name": book.archive.filename if book.archive is not None else None,
-        "archive_type": book.archive.type.name if book.archive is not None else None
-        }
+    with ACBFBook(dir) as book:
+        dir = Path("tests/results") / Path(dir).name
+        os.makedirs(dir, exist_ok=True)
+        op = {
+            "book_path": str(book.book_path),
+            "archive_name": book.archive.filename if book.archive is not None else None,
+            "archive_type": book.archive.type.name if book.archive is not None else None
+            }
     with open(dir / "test_book_props.json", 'w', encoding="utf-8", newline='\n') as result:
         result.write(json.dumps(op, ensure_ascii=False, indent='\t', separators=(', ', ': ')))
 
