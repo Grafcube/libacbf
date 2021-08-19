86635453c4931d2e6aefc67533c4fa54993beb65
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-08-19 17:27:25 +0530

    Finished style tests

--- tests/test_styles.py
+++ tests/test_styles.py
@@ -1,26 +1,22 @@
-import json
 import os
+import json
+import pytest
 from pathlib import Path
-from typing import Tuple
 from libacbf import ACBFBook
+from tests.testres import samples
 
 
-def make_style_dir(path):
-    os.makedirs(path / "styles", exist_ok=True)
-    return path / "styles"
+@pytest.mark.parametrize("dir", samples.values())
+def test_styles(dir, results):
+    dir = Path(dir)
+    op_path = results / "test_styles" / dir.name
+    os.makedirs(op_path / "stylesheets", exist_ok=True)
+    with ACBFBook(dir) as book:
+        with open(op_path / "list_styles.json", 'w', encoding="utf-8", newline='\n') as result:
+            result.write(json.dumps(list(book.styles.list_styles()), ensure_ascii=False, indent='\t',
+                                    separators=(', ', ': ')))
 
-
-def test_styles(read_books: Tuple[Path, ACBFBook]):
-    path, book = read_books
-    dir = make_style_dir(path)
-
-    with open(dir / "test_styles.json", 'w', encoding="utf-8", newline='\n') as result:
-        result.write(json.dumps(list(book.styles.list_styles()), ensure_ascii=False, indent='\t',
-                                separators=(', ', ': ')))
-
-    for i in book.styles.list_styles():
-        name = i
-        if i == '_':
-            name = "embedded.css"
-        with open(dir / name, 'w', encoding="utf-8", newline='\n') as st_output:
-            st_output.write(book.styles[i])
+        for i in book.styles.list_styles():
+            name = "embedded.css" if i == '_' else i
+            with open(op_path / "stylesheets" / name, 'wb') as st_output:
+                st_output.write(book.styles[i])
