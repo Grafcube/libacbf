6af3d4b31dec1ee095b9f59388d45a3f6c6d1f84
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-08-22 14:39:47 +0530

    Updated publish info tests

--- tests/test_editor/test_edit_meta/test_edit_publishinfo.py
+++ tests/test_editor/test_edit_meta/test_edit_publishinfo.py
@@ -1,48 +1,49 @@
-import os
-from pathlib import Path
 from libacbf import ACBFBook
 
-edit_dir = Path("tests/results/edit_meta/publish_info/")
-os.makedirs(edit_dir, exist_ok=True)
+
+def test_publisher(results_edit_publishinfo):
+    with ACBFBook(results_edit_publishinfo / "test_publisher.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Publisher"
+        book.publisher_info.publisher = "Grafcube"
+
+        book.create_placeholders()
 
 
-def test_publisher():
-    with ACBFBook(edit_dir / "test_publisher.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Publisher")
-        book.publisher_info.set_publisher("Grafcube")
+def test_publish_date(results_edit_publishinfo):
+    with ACBFBook(results_edit_publishinfo / "test_publish_date.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Publish Date"
+        book.publisher_info.set_date("1st Jan, 2021")
+
+        book.create_placeholders()
 
 
-def test_publish_date():
-    with ACBFBook(edit_dir / "test_publish_date.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Publish Date")
-        book.publisher_info.set_publish_date("1st Jan, 2021")
+def test_publish_date_excl(results_edit_publishinfo):
+    with ACBFBook(results_edit_publishinfo / "test_publish_date_excl.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Publish Date No ISO"
+        book.publisher_info.set_date("2nd Jan, 2021", include_date=False)
+
+        book.create_placeholders()
 
 
-def test_publish_date_excl():
-    with ACBFBook(edit_dir / "test_publish_date_excl.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Publish Date No ISO")
-        book.publisher_info.set_publish_date("2nd Jan, 2021", include_date=False)
+def test_city(results_edit_publishinfo):
+    with ACBFBook(results_edit_publishinfo / "test_city.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Publish City"
+        book.publisher_info.publish_city = "Earth City"
+
+        book.create_placeholders()
 
 
-def test_city():
-    with ACBFBook(edit_dir / "test_city.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Publish City")
-        book.publisher_info.set_publish_city("Mars City")
-        book.publisher_info.set_publish_city(None)
-        book.publisher_info.set_publish_city("Earth City")
+def test_isbn(results_edit_publishinfo):
+    with ACBFBook(results_edit_publishinfo / "test_isbn.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test ISBN"
+        book.publisher_info.isbn = "123-4-56789-012-3"
+
+        book.create_placeholders()
 
 
-def test_isbn():
-    with ACBFBook(edit_dir / "test_isbn.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test ISBN")
-        book.publisher_info.set_isbn("321-0-98765-432-1")
-        book.publisher_info.set_isbn(None)
-        book.publisher_info.set_isbn("123-4-56789-012-3")
+def test_license(results_edit_publishinfo):
+    with ACBFBook(results_edit_publishinfo / "test_license.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test License"
+        book.publisher_info.license = "Fictional License"
 
-
-def test_license():
-    with ACBFBook(edit_dir / "test_license.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test License")
-        book.publisher_info.set_license("Not real License")
-        book.publisher_info.set_license(None)
-        book.publisher_info.set_license("Fictional License")
+        book.create_placeholders()
