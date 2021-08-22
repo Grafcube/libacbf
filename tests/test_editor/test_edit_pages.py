e62e483be1836771c01643a1b370867ca15f9e4d
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-08-22 12:54:28 +0530

    Updated page edit tests

--- tests/test_editor/test_edit_pages.py
+++ tests/test_editor/test_edit_pages.py
@@ -1,43 +1,20 @@
-import os
-from pathlib import Path
 from libacbf import ACBFBook
 
-edit_dir = Path("tests/results/edit_body/")
-os.makedirs(edit_dir, exist_ok=True)
 
+def test_pages(results_edit_body):
+    with ACBFBook(results_edit_body / "test_pages.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Pages"
 
-def test_pages():
-    with ACBFBook(edit_dir / "test_pages.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Pages")
+        book.body.append_page("page1")
+        book.body.append_page("page2")
+        book.body.append_page("page4")
+        book.body.append_page("REMOVE_ME")
+        book.body.append_page("page5")
+        book.body.insert_page(2, "page3")
 
-        book.body.pages[0].set_image_ref("page1.jpg")
-        for n in range(1, 10):
-            book.body.insert_new_page(n, f"page{n + 1}.jpg")
+        book.body.pages.pop(4)
 
-        book.body.insert_new_page(5, "pg6.png")
-        book.body.insert_new_page(3, "REMOVE ME.png")
-        book.body.insert_new_page(7, "SWAP 2.png")
-        book.body.insert_new_page(1, "SWAP 8.png")
+        book.body.pages[0].set_transition("scroll_right")
 
-        book.body.remove_page(4)
-        book.body.reorder_page(7, 1)
-        book.body.reorder_page(2, 7)
-
-
-def test_transition():
-    with ACBFBook(edit_dir / "test_transition.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Page Transition")
-
-        book.body.pages[0].set_transition("none")
-        book.body.pages[0].set_transition(None)
-        book.body.pages[0].set_transition("scroll_down")
-
-
-def test_title():
-    with ACBFBook(edit_dir / "test_title.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Page Title")
-
-        book.body.pages[0].set_title("A Page")
-        book.body.pages[0].set_title("Test Page", "en")
-        book.body.pages[0].set_title(None, "en")
-        book.body.pages[0].set_title("First Page", "en")
+        book.body.pages[0].title['_'] = "Test Page"
+        book.body.pages[0].title["en"] = "First Page"
