0cf298af51e08313c0398c8d4eada56e1130b66e
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-08-22 13:57:23 +0530

    Updated book info editing tests

--- tests/test_editor/test_edit_meta/test_edit_bookinfo.py
+++ tests/test_editor/test_edit_meta/test_edit_bookinfo.py
@@ -1,177 +1,166 @@
-import os
 import pytest
-from pathlib import Path
 from libacbf import ACBFBook
-from libacbf.metadata import Author
-
-edit_dir = Path("tests/results/edit_meta/book_info/")
-os.makedirs(edit_dir, exist_ok=True)
 
 
-def test_authors():
-    with ACBFBook(edit_dir / "edit_authors.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Authors")
+def test_authors(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_authors.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Authors"
 
         book.book_info.add_author("Test")
         book.book_info.add_author("Hugh", "Mann")
         au = book.book_info.authors[-1]
-        book.book_info.add_author(author=Author("Grafcube"))
-        book.book_info.add_author("Remove", "This")
+        book.book_info.add_author("REMOVE", "ME")
         rem = book.book_info.authors[-1]
         book.book_info.add_author("NotGrafcube")
+        book.book_info.add_author("EDIT_ME")
+        edit_me = book.book_info.authors[-1]
 
-        book.book_info.edit_author(0, first_name="TheFirst", last_name="TheLast", middle_name="TheMid",
-                                   lang="kn", nickname=None, home_page="https://example.com/testing")
+        match_message = "Author must have either First Name and Last Name or Nickname."
+        with pytest.raises(ValueError, match=match_message):
+            au.first_name = None
+        with pytest.raises(ValueError, match=match_message):
+            au.last_name = None
+        with pytest.raises(ValueError, match=match_message):
+            edit_me.nickname = None
 
-        book.book_info.edit_author(au, middle_name=None, lang=None)
+        edit_me.first_name = "TheFirst"
+        edit_me.last_name = "TheLast"
+        edit_me.nickname = None
+        edit_me.middle_name = "TheMid"
+        edit_me.home_page = "https://example.com/authors/someone"
+        edit_me.email = "someone123@example.com"
+        edit_me.lang = "en"
+        edit_me.activity = "Translator"
+        au.nickname = "NotAPlatypus"
 
-        book.book_info.edit_author(0, activity="Translator")
+        book.book_info.authors.remove(rem)
 
-        with pytest.raises(ValueError, match="Author must have either First Name and Last Name or Nickname."):
-            book.book_info.edit_author(au, first_name=None)
-
-        with pytest.raises(AttributeError, match="`Author` has no attribute `something`."):
-            book.book_info.edit_author(au, something="Non existant")
-
-        book.book_info.remove_author(-1)
-        book.book_info.remove_author(rem)
-
-
-def test_titles():
-    with ACBFBook(edit_dir / "edit_titles.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Titles")
-        book.book_info.edit_title("Test Edit Titles", "en")
-        book.book_info.edit_title("ಹೆಸರು", "kn")
-        book.book_info.edit_title("ಹೆಸರು ಪರೀಕ್ಷೆ", "kn")
-        book.book_info.edit_title("タイトル テスト", "jp")
-        book.book_info.remove_title("jp")
-        book.book_info.remove_title()
+        book.create_placeholders()
 
 
-def test_genres():
-    with ACBFBook(edit_dir / "edit_genres.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Genres")
+def test_titles(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_titles.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Titles"
+        book.book_info.book_title["en"] = "Test Edit Titles"
+        book.book_info.book_title["kn"] = "ಹೆಸರು ಪರಿಕ್ಷೆ"
+        book.book_info.book_title.pop('_')
+
+        book.create_placeholders()
+
+
+def test_genres(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_genres.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Genres"
 
         book.book_info.edit_genre("other")
-        book.book_info.edit_genre("non_fiction", 42)
+        book.book_info.edit_genre("horror", 42)
         book.book_info.edit_genre("manga", 11)
-        book.book_info.edit_genre("manga", None)
-        book.book_info.remove_genre("manga")
+        book.book_info.pop_genre("horror")
+
+        assert book.book_info.get_match("manga") == 11
+
+        with pytest.raises(ValueError, match="`match` must be an integer from 0 to 100."):
+            book.book_info.edit_genre("other", -1)
+        with pytest.raises(ValueError, match="`match` must be an integer from 0 to 100."):
+            book.book_info.edit_genre("other", 101)
+
+        book.create_placeholders()
 
 
-def test_annotations():
-    with ACBFBook(edit_dir / "edit_annotations.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Annotations")
+def test_annotations(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_annotations.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Annotations"
 
-        book.book_info.edit_annotation("This is an annotation.")
-        book.book_info.edit_annotation("This is an annotation.\nThis is another line.")
-        book.book_info.edit_annotation("This is an annotation.", "en")
-        book.book_info.edit_annotation("This is an annotation.\nThis is another line.", "en")
-        book.book_info.edit_annotation("ಇದು ಈ ಪುಸ್ತಕದ ವಿವರಣೆ", "kn")
-        book.book_info.edit_annotation("ಇದು ಈ ಪುಸ್ತಕದ ವಿವರಣೆ.\nಇದು ಎರಡನೇ ಸಾಲು.", "kn")
-        book.book_info.remove_annotation()
-        book.book_info.remove_annotation("kn")
+        book.book_info.annotations['_'] = "This is an annotation."
+        book.book_info.annotations["en"] = "This is an annotation.\nThis is another line."
+        book.book_info.annotations["kn"] = "ಇದು ಈ ಪುಸ್ತಕದ ವಿವರಣೆ.\nಇದು ಎರಡನೇ ಸಾಲು."
+        book.book_info.annotations.pop('_')
 
-
-def test_coverpage():
-    with ACBFBook(edit_dir / "edit_coverpage.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Coverpage Fails")
-
-        with pytest.raises(AttributeError, match=r'`coverpage` has no attribute `\w+`\.'):
-            book.book_info.cover_page.set_bgcolor("#ffffff")
-            book.book_info.cover_page.set_transition("fade")
-            book.book_info.cover_page.set_title("Cover title")
+        book.create_placeholders()
 
 
 # --- Optional ---
 
-def test_languagelayers():
-    with ACBFBook(edit_dir / "edit_languages.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Languages")
+def test_languagelayers(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_languages.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Languages"
 
         book.book_info.add_language("en", False)
-        book.book_info.add_language("en", True)
+        book.book_info.add_language("en", False)
         en = book.book_info.languages[-1]
         book.book_info.add_language("kn", False)
         kn = book.book_info.languages[-1]
-        book.book_info.add_language("jp", True)
-        book.book_info.edit_language(kn, show=True)
-        book.book_info.edit_language(-1, lang="ta")
-        book.book_info.remove_language(-1)
-        book.book_info.remove_language(en)
+        book.book_info.add_language("ta", True)
+        book.book_info.languages.pop(-1)
+        en.show = True
+        kn.show = True
+
+        book.create_placeholders()
 
 
-def test_characters():
-    with ACBFBook(edit_dir / "edit_characters.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Characters")
+def test_characters(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_characters.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Characters"
 
-        book.book_info.add_character("Test")
-        book.book_info.add_character("Testing")
-        book.book_info.add_character("Another")
-        book.book_info.add_character("And Another")
-        book.book_info.remove_character("Testing")
-        book.book_info.remove_character(-1)
+        book.book_info.characters.extend(["Testing", "REMOVE_ME", "Another", "And Another"])
+        book.book_info.characters.remove("REMOVE_ME")
+
+        book.create_placeholders()
 
 
-def test_keywords():
-    with ACBFBook(edit_dir / "edit_keywords.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Keywords")
+def test_keywords(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_keywords.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Keywords"
 
-        book.book_info.add_keywords("ebook", "tag", "comic book", "Tag", "TAG")
-        book.book_info.add_keywords("ebook", "tag", "comic book", "Tag", "TAG", lang="en")
-        book.book_info.remove_keyword("science fiction", "TaG", lang="en")
-        book.book_info.clear_keywords()
+        book.book_info.keywords['_'] = {"ebook", "tag", "comic book", "tag"}
+        book.book_info.keywords["en"] = {"ebook", "tag", "comic book", "tag"}
+        book.book_info.keywords["kn"] = {"ಪುಸ್ತಕ", "ಹೆಸರು", "ಕಾಮಿಕ್ ಪುಸ್ತಕ", "ಹೆಸರು"}
+        book.book_info.keywords.pop('_')
 
-
-def test_series():
-    with ACBFBook(edit_dir / "edit_series.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Series")
-
-        try:
-            book.book_info.edit_series("Some Comics")
-        except AttributeError as e:
-            if str(e) == "`sequence` cannot be blank for new series entry `Some Comics`.":
-                pass
-            else:
-                raise e
-
-        book.book_info.edit_series("Some Comics", 2)
-        book.book_info.edit_series("More Comics", 5)
-        book.book_info.edit_series("No Comics", 0)
-        book.book_info.edit_series("Some Comics", volume=1)
-        book.book_info.edit_series("More Comics", volume=2)
-        book.book_info.edit_series("Some Comics", volume=None)
-        book.book_info.remove_series("No Comics")
+        book.create_placeholders()
 
 
-def test_rating():
-    with ACBFBook(edit_dir / "edit_rating.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Content Rating")
+def test_series(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_series.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Series"
 
-        book.book_info.edit_content_rating("16+")
-        book.book_info.edit_content_rating("16+", "Age Rating")
-        book.book_info.edit_content_rating("16+", "Another Age Rating")
-        book.book_info.edit_content_rating("16+", "No Age Rating")
-        book.book_info.edit_content_rating("17+", "Another Age Rating")
-        book.book_info.remove_content_rating()
-        book.book_info.remove_content_rating("No Age Rating")
+        book.book_info.add_series("Some Comics", 2)
+        book.book_info.add_series("More Comics", 5, 1)
+        book.book_info.add_series("Even More Comics", 7, 2)
+        book.book_info.add_series("No Comics", 0)
+        book.book_info.series["Even More Comics"].volume = None
+        book.book_info.series.pop("No Comics")
+
+        book.create_placeholders()
 
 
-def test_dbref():
-    with ACBFBook(edit_dir / "edit_dbref.acbf", 'w', archive_type=None) as book:
-        book.book_info.edit_title("Test Edit Database Reference")
+def test_rating(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_rating.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Content Rating"
 
-        book.book_info.add_database_ref("ComicSite", "123456")
-        cs = book.book_info.database_ref[-1]
-        book.book_info.add_database_ref("ComicSite", "123456")
+        book.book_info.content_rating["Age Rating"] = "16+"
+        book.book_info.content_rating["DC Comics rating system"] = "T+"
+        book.book_info.content_rating["Marvel Comics rating system"] = "PARENTAL ADVISORY"
+        book.book_info.content_rating["REMOVE_ME"] = '0'
+        book.book_info.content_rating.pop("REMOVE_ME")
+
+        book.create_placeholders()
+
+
+def test_dbref(results_edit_bookinfo):
+    with ACBFBook(results_edit_bookinfo / "edit_dbref.acbf", 'w', archive_type=None) as book:
+        book.book_info.book_title['_'] = "Test Edit Database Reference"
+
+        book.book_info.add_dbref("ComicSite", "123456")
+        book.book_info.add_dbref("ComicSite", "123456")
         csu = book.book_info.database_ref[-1]
-        book.book_info.add_database_ref("AnotherSite", "654321", "id")
-        an = book.book_info.database_ref[-1]
-        book.book_info.add_database_ref("NoSite", "654321")
+        book.book_info.add_dbref("NoSite", "000000")
         ns = book.book_info.database_ref[-1]
-        book.book_info.add_database_ref("NoSite", "id/654321")
-        book.book_info.edit_database_ref(cs, type="id")
-        book.book_info.edit_database_ref(csu, ref="https://example.com/comicsite/id/123456", type="URL")
-        book.book_info.edit_database_ref(an, type=None)
-        book.book_info.remove_database_ref(-1)
-        book.book_info.remove_database_ref(ns)
+        book.book_info.add_dbref("AnotherSite", "654321", "ID")
+
+        csu.type = "URL"
+        csu.reference = "https://example.com/comicsite/id/123456"
+
+        book.book_info.database_ref.remove(ns)
+
+        book.create_placeholders()
