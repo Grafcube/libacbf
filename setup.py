54045e7d0744cf7eb9f6333d38ace73560fec9e3
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-08-24 13:26:28 +0530

    Update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import sys
 from setuptools import setup, find_packages
 
-version = "1.0.0"
+version = "1.0.1"
 version_config = {"version_config": True}
 if sys.argv[1] == "version":
     version += sys.argv[2]
